dfa1552a1fa288ad564319148b1a2b1e4bf1fed9
RidzBuilder <argopatmo@gmail.com>
2026-09-20 11:51:29 +0700

    Add lib/core/constants/collections.dart

---
+++ lib/core/constants/collections.dart
@@ -0,0 +1,7 @@
+class Collections {
+  static const String users = 'users';
+  static const String stores = 'stores';
+  static const String teamMembers = 'team_members';
+  static const String programs = 'programs';
+  static const String reports = 'reports';
+}
